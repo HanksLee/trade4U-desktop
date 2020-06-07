553e81c20b77676259a9d8c0341444d8ba690544
TY <terenyeung@gmail.com>
2020-06-07 23:55:08 +0800

    [M] update prod of API

--- src/utils/request.ts
+++ src/utils/request.ts
@@ -82,7 +82,7 @@ export default class API implements IAPI {
 const apiMap = {
   dev: "/api/moon/api",
   qa: "http://api.cangshu360.com/api",
-  prod: "http://api.cangshu360.com/api",
+  prod: "http://api.trading8a.com/api",
 };
 
 export const moonAPI = new API({
