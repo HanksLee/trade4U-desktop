ede4c6a28c2fa2b2940746e93fd4625aa197c270
Hank Lee <baf6715@hotmail.com.tw>
2020-06-08 16:44:30 +0800

    adjust websock domain in prod mode

--- src/utils/ws.ts
+++ src/utils/ws.ts
@@ -3,13 +3,13 @@ import utils from "utils";
 const wsMap = {
   dev: "cangshu360.com",
   qa: "cangshu360.com",
-  prod: "trading8a.com",
+  prod: "trading8a.com"
 };
 
 export default function ws(path) {
   const token = utils.getLStorage("MOON_DESKTOP_TOKEN");
 
   return new WebSocket(
-    `ws://stock-${wsMap[process.env.MODE]}/ws/trader/${path}?token=${token}`
+    `ws://stock-ws.${wsMap[process.env.MODE]}/ws/trader/${path}?token=${token}`
   );
 }
